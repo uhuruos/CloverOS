c0bbf4acf2dce5e568c60934d552b5eafcb16816
James Bond <jamesbond@007.uk>
2017-06-07 21:32:58 +0000

    Fix install script

--- installscript.sh
+++ installscript.sh
@@ -1,3 +1,7 @@
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 read -p "Enter preferred root password " rootpassword
 read -p "Enter preferred username " user
 read -p "Enter preferred user password " userpassword
