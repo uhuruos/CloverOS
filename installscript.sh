23b5c153fc92dff50f5a4add267d3497d0bcdc76
James Bond <jamesbond@007.uk>
2017-06-08 20:51:04 +0000

    Added torrent client for downloading anime

--- installscript.sh
+++ installscript.sh
@@ -51,7 +51,7 @@ echo -e "$userpassword\n$userpassword" | passwd user
 gpasswd -a $user wheel
 emerge -1 openssh openssl
 echo "media-video/mpv ~amd64" >> /etc/portage/package.accept_keywords
-emerge xorg-server twm feh aterm sudo xfe wpa_supplicant dash porthole firefox emacs gimp mpv smplayer linux-firmware alsa-utils
+emerge xorg-server twm feh aterm sudo xfe wpa_supplicant dash porthole firefox emacs gimp mpv smplayer rtorrent linux-firmware alsa-utils
 rm -Rf /usr/portage/packages/*
 sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
 sed -Ei "s/^c([2-6]):2345/#\0/" /etc/inittab
