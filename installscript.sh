0f7bfb792064f764beee2732a8a44eb9c57d4387
James Bond <jamesbond@007.uk>
2017-06-08 20:57:52 +0000

    Added torrent client for downloading anime and riced config

--- installscript.sh
+++ installscript.sh
@@ -72,6 +72,9 @@ wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/.mpv/c
 eselect fontconfig enable 52-infinality.conf
 eselect infinality set infinality
 eselect lcdfilter set infinality
+echo 'session = /home/$user/.rtorrent\ndirectory = /home/$user/Downloads/\nport_range = 53165-62153\ndht = on\npeer_exchange = yes\nuse_udp_trackers = yes' > .rtorrent.rc
+mkdir Downloads
+mkdir .rtorrent
 
 exit
 
