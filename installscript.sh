a66f3bfb1b06e701c08ba08ea5bcb5c4ff0fb98c
James Bond <jamesbond@007.uk>
2017-06-12 12:29:41 +0000

    Fixes #4

--- installscript.sh
+++ installscript.sh
@@ -47,7 +47,7 @@ rc-update add dhcpcd default
 
 echo -e "$rootpassword\n$rootpassword" | passwd
 useradd $user
-echo -e "$userpassword\n$userpassword" | passwd user
+echo -e "$userpassword\n$userpassword" | passwd $user
 gpasswd -a $user wheel
 
 emerge -1 openssh openssl
