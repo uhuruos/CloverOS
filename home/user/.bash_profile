0b65c0f10ce382a0eaf4e60f266a0a4ee5938742
James Bond <jamesbond@007.uk>
2017-06-14 12:58:18 +0000

    Added livecd install script

--- home/user/.bash_profile
+++ home/user/.bash_profile
@@ -8,6 +8,3 @@ if [ -z "$DISPLAY" ]; then
        feh --bg-max wallpaper.png
     fi
 fi
-
-# color layout for bash sessions with git integration, currently broken
-#export PS1='\[\033[01;30m\]\t `if [ $? = 0 ]; then echo "\[\033[01;32m\]ツ"; else echo "\[\033[01;31m\]✗"; fi` \[\033[00;32m\]\h\[\033[00;37m\]:\[\033[31m\]$(__git_ps1 "(%s)\[\033[01m\]")\[\033[00;34m\]\w\[\033[00m\] >'
---
+++ make_livecd.sh
@@ -0,0 +1,86 @@
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+rootpassword=password
+user=user
+userpassword=userpassword
+
+mkdir gentoo
+cd gentoo
+
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-20170525.tar.bz2
+tar pxf stage3*
+rm -f stage3*
+
+cp /etc/resolv.conf etc
+mount -t proc none proc
+mount --rbind /dev dev
+mount --rbind /sys sys
+
+cat << EOF | chroot .
+
+emerge-webrsync
+
+echo -e '\nPORTAGE_BINHOST="https://cloveros.ga"\nMAKEOPTS="-j8"\nEMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"\nCFLAGS="-O3 -pipe -march=native"\nCXXFLAGS="${CFLAGS}"' >> /etc/portage/make.conf
+
+wget -O - https://raw.githubusercontent.com/chiru-no/cloveros/master/kernel.tar.xz | tar xJ -C /boot/
+mkdir /lib/modules/
+wget -O - https://raw.githubusercontent.com/chiru-no/cloveros/master/modules.tar.xz | tar xJ -C /lib/modules/
+
+emerge grub dhcpcd
+
+grub-install /dev/$drive
+grub-mkconfig > /boot/grub/grub.cfg
+
+rc-update add dhcpcd default
+
+echo -e "$rootpassword\n$rootpassword" | passwd
+useradd $user
+echo -e "$userpassword\n$userpassword" | passwd $user
+gpasswd -a $user wheel
+
+emerge -1 openssh openssl
+echo "media-video/mpv ~amd64" >> /etc/portage/package.accept_keywords
+emerge xorg-server twm feh aterm sudo xfe wpa_supplicant dash porthole firefox emacs gimp mpv smplayer rtorrent weechat conky linux-firmware alsa-utils rxvt-unicode zsh zsh-completions gentoo-zsh-completions inconsolata bind-tools colordiff xdg-utils
+rm -Rf /usr/portage/packages/*
+sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
+sed -Ei "s@c([2-6]):2345:respawn:/sbin/agetty 38400 tty@#\0@" /etc/inittab
+sed -i "s@c1:12345:respawn:/sbin/agetty 38400 tty1 linux@c1:12345:respawn:/sbin/agetty --noclear 38400 tty1 linux@" /etc/inittab
+sed -i "s/set timeout=5/set timeout=0/" /boot/grub/grub.cfg
+echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel\nupdate_config=1" > /etc/wpa_supplicant/wpa_supplicant.conf
+rc-update add alsasound default
+rc-update add wpa_supplicant default
+eselect fontconfig enable 52-infinality.conf
+eselect infinality set infinality
+eselect lcdfilter set infinality
+gpasswd -a $user audio
+gpasswd -a $user video
+cd /home/$user/
+rm .bash_profile
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/.bash_profile
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/.zshrc
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/.twmrc
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/.Xdefaults
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/wallpaper.png
+echo -e "session = /home/$user/.rtorrent\ndirectory = /home/$user/Downloads/\nport_range = 53165-62153\ndht = on\npeer_exchange = yes\nuse_udp_trackers = yes" > .rtorrent.rc
+mkdir Downloads
+mkdir .rtorrent
+mkdir .mpv
+cd .mpv
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/home/user/.mpv/config
+chown -R $user /home/$user/
+
+emerge gparted
+sed -i "s@c1:12345:respawn:/sbin/agetty --noclear 38400 tty1 linux@c1:12345:respawn:/sbin/agetty -a user --noclear 38400 tty1 linux@" /etc/inittab
+sed -i "s@twm\&@twm\&\nurxvt -e sudo ./installscript.sh \&@" /home/user/.bash_profile
+wget https://raw.githubusercontent.com/chiru-no/cloveros/master/livecd_installer.txt -O /home/user/installscript.sh
+chmod +x /home/user/installscript.sh
+
+exit
+
+EOF
+
+cd ..
+mksquashfs gentoo image.squashfs -b 1024k -comp xz
