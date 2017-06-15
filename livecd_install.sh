d55e263e31d1cccae8754849981862d61f958db8
James Bond <jamesbond@007.uk>
2017-06-15 22:35:30 +0000

    Add gparted to livecd installer

--- livecd_install.sh
+++ livecd_install.sh
@@ -9,6 +9,7 @@ if [[ $partitioning = "a" ]]; then
     read -e -p "Enter drive for CloverOS installation: " -i "/dev/sda" drive
     partition=${drive}1
 elif [[ $partitioning = "m" ]]; then
+    sudo gparted&
     read -e -p "Enter partition for CloverOS installation: " -i "/dev/sda1" partition
     drive=${partition%"${partition##*[!0-9]}"}
 else
