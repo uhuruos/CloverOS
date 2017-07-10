4af9881bd07a97281c424a081248a15afd4c4ab9
James Bond <jamesbond@007.uk>
2017-07-10 00:11:16 +0000

    homedirectory/script: cloveros_settings.sh made

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -12,15 +12,15 @@ read -p "Select option: " -n 1 choice
 echo
 if [[ $choice = "1" ]]; then
    mirrors=("useast.cloveros.ga" "uswest.cloveros.ga" "fr.cloveros.ga")
-    i=1
-    while [[ $i -le ${#mirrors[@]} ]]; do
-        echo "$i) ${mirrors[$i-1]}"
-        let i=i+1
+    i=0
+    for i in "${!mirrors[@]}"; do
+        echo "$(($i+1))) ${mirrors[$i]}"
     done
     read -p "Select mirror: " -n 1 choicemirror
-    sed -i "s@PORTAGE_BINHOST=\".*\"@PORTAGE_BINHOST=\"https://${mirrors[$choicemirror-1]}\"@" /etc/portage/make.conf
+    choicemirror=${choicemirror}-1
+    sed -i "s@PORTAGE_BINHOST=\".*\"@PORTAGE_BINHOST=\"https://${mirrors[$choicemirror]}\"@" /etc/portage/make.conf
     echo
-    echo "Mirror changed to: ${mirrors[$choicemirror-1]}"
+    echo "Mirror changed to: ${mirrors[$choicemirror]}"
     exit 0
 elif [[ $choice = "2" ]]; then
 echo
@@ -34,4 +34,3 @@ else
     echo "Invalid option."
     exit 1
 fi
-
