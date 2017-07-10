3610521e9498ceb0f651bf6cb8f9b5254e020f9f
James Bond <jamesbond@007.uk>
2017-07-10 13:52:55 +0000

    quickpkg.sh: added package signing

--- quickpkg.sh
+++ quickpkg.sh
@@ -7,7 +7,6 @@ quickpkg --include-unmodified-config=y "*/*" | ansi2html | tail -n +1081 | head 
 EIX_LIMIT=0 eix --installed -F | grep -v "Available versions" | ansi2html > s/packages.html
 emerge --buildpkgonly vnstat sudo openssh dnscrypt-proxy
 mv s/ /usr/portage/packages/
-chmod -R 755 /usr/portage/packages/
 cd /usr/portage/packages/s/
 php website.php
 
@@ -21,6 +20,7 @@ mv usr/portage/packages/* .
 rm -Rf usr
 cd ..
 
+chmod -R 755 /usr/portage/packages/
 sudo -u gentoo rsync -av --delete-after /usr/portage/packages/ root@useast.cloveros.ga:/var/www/htdocs/useast.cloveros.ga/
 sudo -u gentoo rsync -av --delete-after /usr/portage/packages/ root@uswest.cloveros.ga:/var/www/htdocs/uswest.cloveros.ga/
 sudo -u gentoo rsync -av --delete-after /usr/portage/packages/ root@fr.cloveros.ga:/var/www/html/
