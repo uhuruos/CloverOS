0b8eba37d63705b26529324995827637bf4e9902
James Bond <jamesbond@007.uk>
2017-07-10 14:22:48 +0000

    quickpkg.sh: added package signing

--- quickpkg.sh
+++ quickpkg.sh
@@ -22,6 +22,6 @@ rmdir * */* &> /dev/null
 cd ..
 
 chmod -R 755 /usr/portage/packages/
-sudo -u gentoo rsync -av --delete-after /usr/portage/packages/ root@useast.cloveros.ga:/var/www/htdocs/useast.cloveros.ga/
-sudo -u gentoo rsync -av --delete-after /usr/portage/packages/ root@uswest.cloveros.ga:/var/www/htdocs/uswest.cloveros.ga/
-sudo -u gentoo rsync -av --delete-after /usr/portage/packages/ root@fr.cloveros.ga:/var/www/html/
+rsync -av --delete-after /usr/portage/packages/ root@useast.cloveros.ga:/var/www/htdocs/useast.cloveros.ga/
+rsync -av --delete-after /usr/portage/packages/ root@uswest.cloveros.ga:/var/www/htdocs/uswest.cloveros.ga/
+rsync -av --delete-after /usr/portage/packages/ root@fr.cloveros.ga:/var/www/html/
