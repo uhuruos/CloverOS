705ea6210aed338a663aba9a24f934ea12cc07c7
James Bond <jamesbond@007.uk>
2017-07-16 16:51:23 +0000

    cloveros_settings.sh: cleanup

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -29,30 +29,30 @@ case "$choice" in
 		done
 		read -erp "Select mirror: " -n 1 choicemirror
 		sudo sed -i "s@PORTAGE_BINHOST=\".*\"@PORTAGE_BINHOST=\"https://${mirrors[$choicemirror-1]}\"@" /etc/portage/make.conf
-		echo
-		echo "Mirror changed to: ${mirrors[choicemirror-1]}"
+		echo -e "\nMirror changed to: ${mirrors[choicemirror-1]}."
 		;;
 
 	2)
 		grep " \[" /proc/asound/cards
 		read -erp "Select the audio device to become default: " -n 1 choiceaudio
 		echo -e "defaults.pcm.card ${choiceaudio}\ndefaults.ctl.card ${choiceaudio}" > ~/.asoundrc
+		echo -e "\nAudio device ${choiceaudio} is now the default for ALSA programs."
 		;;
 
 	3)
 		wget -O - https://cloveros.ga/s/kernel.tar.xz | sudo tar xJ -C /boot/
 		wget -O - https://cloveros.ga/s/modules.tar.xz | sudo tar xJ -C /lib/modules/
 		sudo grub-mkconfig -o /boot/grub/grub.cfg
-		echo "Kernel upgraded."
+		echo -e "\nKernel upgraded."
 		;;
 
 	4)
 		if grep -q 'EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"' /etc/portage/make.conf; then
 			sudo sed -i 's/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2"/' /etc/portage/make.conf
-			echo "emerge will now install from source."
+			echo -e "\nemerge will now install from source."
 		else
 			sudo sed -i 's/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2"/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"/' /etc/portage/make.conf
-			echo "emerge will now install from binary."
+			echo -e "\nemerge will now install from binary."
 		fi
 		;;
 
@@ -61,18 +61,18 @@ case "$choice" in
 		backupdir=backup$(< /dev/urandom tr -dc 0-9 | head -c 5)
 		mkdir $backupdir
 		mv .bash_profile .zprofile .zshrc .twmrc .Xdefaults wallpaper.png .xbindkeysrc screenfetch-dev bl.sh cloveros_settings.sh .emacs .emacs.d .twm .rtorrent.rc .mpv .config/xfe/ $backupdir/
-		wget -q "$fileprefix/{.bash_profile,.zprofile,.zshrc,.twmrc,.Xdefaults,wallpaper.png,.xbindkeysrc,screenfetch-dev,bl.sh,cloveros_settings.sh,.emacs,.rtorrent.rc}"
+		wget -q "$fileprefix"/{.bash_profile,.zprofile,.zshrc,.twmrc,.Xdefaults,wallpaper.png,.xbindkeysrc,screenfetch-dev,bl.sh,cloveros_settings.sh,.emacs,.rtorrent.rc}
 		chmod +x screenfetch-dev bl.sh cloveros_settings.sh
 		mkdir -p .emacs.d/backups
 		mkdir .emacs.d/autosaves
 		mkdir .twm
-		wget -q "$fileprefix/.twm/{minimize.xbm,maximize.xbm,close.xbm}" -P .twm
+		wget -q "$fileprefix"/.twm/{minimize.xbm,maximize.xbm,close.xbm} -P .twm
 		mkdir -p .config/xfe/
-		wget -q "$fileprefix/.config/xfe/xferc" -P .config/xfe
+		wget -q "$fileprefix"/.config/xfe/xferc -P .config/xfe
 		sed -i "s@/home/user/@/home/$USER/@" .rtorrent.rc
 		mkdir .mpv
-		wget -q "$fileprefix/.mpv/config" -P .mpv
-		echo "Configuration updated to new CloverOS defaults, old settings are moved to ~/$backupdir/"
+		wget -q "$fileprefix"/.mpv/config -P .mpv
+		echo -e "\nConfiguration updated to new CloverOS defaults, old settings are moved to ~/$backupdir/"
 		;;
 
 	6)
@@ -80,46 +80,46 @@ case "$choice" in
 			sudo emerge ntp
 		fi
 		sudo ntpdate pool.ntp.org
-		echo "Time synced."
+		echo -e "\nTime synced."
 		;;
 
 	7)
 		echo -e "Available timezones: $(ls -1 /usr/share/zoneinfo/ | tr "\n" " ") \n"
 		read -erp "Select a timezone: " timezone
 		sudo cp /usr/share/zoneinfo/${timezone} /etc/localtime
-		echo "Timezone set to ${timezone}."
+		echo -e "\nTimezone set to ${timezone}."
 		;;
 
 	8)
 		rm -Rf /usr/portage/packages/*
 		rm -Rf /tmp/curlcache/*
-		echo "Package cache cleared."
+		echo -e "\nPackage cache cleared."
 		;;
 
 	9)
 		if ! grep -Fq 'FETCHCOMMAND_HTTPS="/home/'$USER'/curlcache.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""' /etc/portage/make.conf; then
 			echo 'FETCHCOMMAND_HTTPS="/home/'$USER'/curlcache.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""' | sudo tee -a /etc/portage/make.conf
-			wget "$fileprefix/curlcache.sh" -O ~/curlcache.sh
+			wget "$fileprefix"/curlcache.sh -O ~/curlcache.sh
 			chmod +x ~/curlcache.sh
 			if ! type /usr/bin/gpg > /dev/null; then
 				sudo emerge gnupg
 			fi
 			if ! sudo gpg --list-keys "78F5 AC55 A120 07F2 2DF9 A28A 78B9 3F76 B8E4 2805"; then
 				sudo gpg --keyserver keys.gnupg.net --recv-key "78F5 AC55 A120 07F2 2DF9 A28A 78B9 3F76 B8E4 2805"
-				echo
 			fi
-			echo "Validation enabled; emerge will now check if Packages is outdated before redownloading."
+			echo -e "\nValidation enabled; emerge will now check if Packages is outdated before redownloading."
 		else
 			sudo sed -i '/FETCHCOMMAND_HTTPS/d' /etc/portage/make.conf
-			echo "Validation disabled; emerge will now redownload Packages every time."
+			echo -e "\nValidation disabled; emerge will now redownload Packages every time."
 		fi
 		;;
 
 	0)
 		cd ~
 		rm cloveros_settings.sh
-		wget "$fileprefix/cloveros_settings.sh"
+		wget "$fileprefix"/cloveros_settings.sh
 		chmod +x cloveros_settings.sh
+		echo -e "\ncloveros_settings.sh is now updated."
 		;;
 
 	*)
