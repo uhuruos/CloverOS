68a3b599ed3df552077fc104875bba4c16a40746
James Bond <jamesbond@007.uk>
2017-07-16 18:47:16 +0000

    cloveros_settings.sh: cleanup

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -97,15 +97,15 @@ case "$choice" in
 
 	9)
 		if ! grep -Fq 'FETCHCOMMAND_HTTPS="/home/'$USER'/curlcache.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""' /etc/portage/make.conf; then
-			echo 'FETCHCOMMAND_HTTPS="/home/'$USER'/curlcache.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""' | sudo tee -a /etc/portage/make.conf
-			wget "$fileprefix"/curlcache.sh -O ~/curlcache.sh
-			chmod +x ~/curlcache.sh
 			if ! type /usr/bin/gpg > /dev/null; then
 				sudo emerge gnupg
 			fi
 			if ! sudo gpg --list-keys "78F5 AC55 A120 07F2 2DF9 A28A 78B9 3F76 B8E4 2805"; then
 				sudo gpg --keyserver keys.gnupg.net --recv-key "78F5 AC55 A120 07F2 2DF9 A28A 78B9 3F76 B8E4 2805"
 			fi
+			echo 'FETCHCOMMAND_HTTPS="/home/'$USER'/curlcache.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""' | sudo tee -a /etc/portage/make.conf
+			wget "$fileprefix"/curlcache.sh -O ~/curlcache.sh
+			chmod +x ~/curlcache.sh
 			echo -e "\nValidation enabled; emerge will now check if Packages is outdated before redownloading."
 		else
 			sudo sed -i '/FETCHCOMMAND_HTTPS/d' /etc/portage/make.conf
