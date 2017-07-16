93289b25ccec77e212979640823c54e255bd1054
James Bond <jamesbond@007.uk>
2017-07-16 11:49:08 +0000

    curlcache.sh: hashes package name only

--- home/user/curlcache.sh
+++ home/user/curlcache.sh
@@ -8,7 +8,8 @@ cachedir=/tmp/curlcache
 
 url="$1"
 dest="$2"
-name="$(sha1sum <<< "$url" | cut -d' ' -f1)"
+package=$(echo $url | sed 's@\(.*\)/\(.*\)/\(.*\)@\2/\3@;')
+name="$(sha1sum <<< "$package" | cut -d' ' -f1)"
 cachef="$cachedir/$name"
 
 mkdir -p "$cachedir"
@@ -21,8 +22,7 @@ fi
 
 ln -sf "$cachef" "$dest"
 
-siglocation=$(echo $1 | sed 's@https://cloveros.ga/@@')
-curl -z "$cachef.asc" -s https://cloveros.ga/s/signatures/$siglocation.asc -o "$cachef.asc"
+curl -z "$cachef.asc" -s https://cloveros.ga/s/signatures/$package.asc -o "$cachef.asc"
 if ! gpg --verify $cachef.asc "$cachef"; then
 	echo "Package failed validation - this package is corrupt."
 fi
