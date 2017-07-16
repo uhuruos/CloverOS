541e1e84fb059fd277487894c44c6de8ebb595db
James Bond <jamesbond@007.uk>
2017-07-16 12:12:05 +0000

    curlcache.sh: fix hash

--- home/user/curlcache.sh
+++ home/user/curlcache.sh
@@ -8,7 +8,8 @@ cachedir=/tmp/curlcache
 
 url="$1"
 dest="$2"
-package=$(echo $url | sed 's@https://[^/]*/@@')
+package=$(echo $url | awk -F/ '{print $4"/"$5}')
+mirror=$(echo $url | awk -F/ '{print $3}')
 name="$(sha1sum <<< "$package" | cut -d' ' -f1)"
 cachef="$cachedir/$name"
 
@@ -22,7 +23,7 @@ fi
 
 ln -sf "$cachef" "$dest"
 
-curl -z "$cachef.asc" -s https://cloveros.ga/s/signatures/$package.asc -o "$cachef.asc"
-if ! gpg --verify $cachef.asc "$cachef"; then
+curl -z "$cachef".asc -s https://"$mirror"/s/signatures/"$package".asc -o "$cachef".asc
+if ! gpg --verify "$cachef".asc "$cachef"; then
 	echo "Package failed validation - this package is corrupt."
 fi
