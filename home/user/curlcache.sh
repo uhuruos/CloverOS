c76b2eb8f8e8d4c4b1ccb9468e3e7b92074c433b
James Bond <jamesbond@007.uk>
2017-07-16 10:04:02 +0000

    cloveros_settings.sh/curlcache.sh: impelemented package validation

--- home/user/curlcache.sh
+++ home/user/curlcache.sh
@@ -23,8 +23,6 @@ ln -sf "$cachef" "$dest"
 
 siglocation=$(echo $1 | sed 's@https://cloveros.ga/@@')
 curl -s https://cloveros.ga/s/signatures/$siglocation.asc -o $cachef.asc
-retval=$?
-gpg --verify $cachef.asc $cachef
-if [ $retval -ne 0 ]; then
+if ! gpg --verify $cachef.asc $cachef; then
 	echo "Package failed validation - this package is corrupt."
 fi
