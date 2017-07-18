4ff3d911c1ca1c4661af66655c8589ba6353c6f2
James Bond <jamesbond@007.uk>
2017-07-18 18:30:45 +0000

    stats.sh: clean output

--- home/user/stats.sh
+++ home/user/stats.sh
@@ -2,7 +2,6 @@
 while :
 do
 topoutput=$(top -b -n2 | grep -E '(load average|Tasks|Cpu|KiB Mem|KiB Swap)' | sed 's/ min//' | tr '\n' ' ')
-netoutput=$(netstat -ei | grep -E '(RX packets|TX packets)')
 clr1="\033[0;37m"
 clr2="\033[0;34m"
 echo -e "
@@ -12,8 +11,8 @@ echo -e "
 "$clr1"Active:"$clr2" $(echo $topoutput | awk '{print $16}') 
 "$clr1"Cpu:"$clr2" $(echo $topoutput | awk '{print 100 - $93}')% 
 "$clr1"Mem:"$clr2" $(echo $topoutput | awk '{print int($48 / 1024)}') MiB / $(echo $topoutput | awk '{printf("%.2f\n", $44 / 1048576)}') GiB 
-"$clr1"Net in:"$clr2" $(echo $netoutput | grep 'RX packets' | awk '{sum += $5} END {print int(sum / 1024)}') MiB 
-"$clr1"Net out:"$clr2" $(echo $netoutput | grep 'TX packets' | awk '{sum += $5} END {print int(sum / 1024)}') MiB 
+"$clr1"Net in:"$clr2" $(netstat -ei | grep 'RX packets' | awk '{sum += $5} END {print int(sum / 1048576)}') MiB 
+"$clr1"Net out:"$clr2" $(netstat -ei | grep 'TX packets' | awk '{sum += $5} END {print int(sum / 1048576)}') MiB 
 "$clr1"Battery: "$clr2"$(cat /sys/class/power_supply/BAT0/capacity)% 
 "$clr1"Brightness:"$clr2" $(awk "BEGIN{print $(cat /sys/class/backlight/*/actual_brightness) / $(cat /sys/class/backlight/*/max_brightness) * 100}")% 
 "$clr1"Volume:"$clr2" $(amixer | tr '\n' ' ' | awk -F '[][]' '{print $2}') 
