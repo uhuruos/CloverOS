5ce93a9dbc2cff7294195ceb7b2072d0fda1f9ee
James Bond <jamesbond@007.uk>
2017-07-22 16:02:25 +0000

    stats.sh: no commands

--- home/user/stats.sh
+++ home/user/stats.sh
@@ -1,47 +1,100 @@
 #!/bin/bash
 tput civis
+cpulasttotal=0
+cpulastidle=0
 while :
 do
-mapfile -t toparray <<< "$(top -b -n2 | grep -E '(top - |Tasks: |Cpu\(s\)|KiB Mem|KiB Swap)')"
-mapfile -t volume <<< "$(amixer sget Master)"
-IFS=' ' read -ra topline1 <<< ${toparray[0]}
-IFS=' ' read -ra topline2 <<< ${toparray[1]}
-IFS=' ' read -ra topline3 <<< ${toparray[7]}
-IFS=' ' read -ra topline4 <<< ${toparray[3]}
-IFS=' ' read -ra topline5 <<< ${toparray[4]}
-uptime="${topline1[4]} ${topline1[5]}"
-uptime=${uptime:0:-1}
-uptime=${uptime%, }
-cpuidle=${topline3[7]}
-cpuidle=${cpuidle%.*}
-mapfile -t netdev <<< "$(</proc/net/dev)"
+system=$(</proc/version)
+system=${system%% (*}
+system=${system/ version/}
+
+uptime=$(</proc/uptime)
+uptime=${uptime%%.*}
+hrs=$((uptime/3600))
+min=$((uptime/60%60))
+uptime="$hrs:$min"
+
+processes=()
+for line in /proc/*[0-9]; do
+    processes+=($line)
+done
+processes=${#processes[@]}
+
+mapfile -t procstat < /proc/stat
+activeprocesses=${procstat[-3]}
+activeprocesses=${activeprocesses/procs_running /}
+
+IFS=' ' read -ra cpustats <<< ${procstat[0]}
+cpustats=("${cpustats[@]:1}")
+cpuidle=${cpustats[3]}
+cputotal=0
+for i in "${cpustats[@]}"; do
+    cputotal=$(($cputotal+$i))
+done
+cpuusage=$(((1000*(($cputotal-$cpulasttotal)-($cpuidle-$cpulastidle))/($cputotal-$cpulasttotal)+5)/10))%
+cpulasttotal=$cputotal
+cpulastidle=$cpuidle
+
+mapfile -t meminfo < /proc/meminfo
+memtotal=${meminfo[0]}
+memtotal=${memtotal#* }
+memtotal=${memtotal/ kB/}
+memory[0]=${meminfo[1]} #memfree
+memory[1]=${meminfo[3]} #buffers
+memory[2]=${meminfo[4]} #cached
+memory[3]=${meminfo[20]} #shmem
+memory[4]=${meminfo[22]} #sreclaimable
+memused=$memtotal
+for line in "${memory[@]}"; do
+    line=${line#* }
+    line=${line/ kB/}
+    memused=$(($memused-$line))
+done
+meminfo=$(($memused/1024))\ MiB\ \/\ $(($memtotal/1024))\ MiB
+
+mapfile -t netdev < /proc/net/dev
 netdev=${netdev[-1]}
 IFS=' ' read -ra netdev <<< ${netdev}
+netin=$((${netdev[1]}/1048576))\ MiB
+netout=$((${netdev[9]}/1048576))\ MiB
+
+mapfile -t volume <<< "$(amixer sget Master)"
 volume=${volume[-1]}
 IFS=' ' read -ra volume <<< ${volume}
 volume=${volume[3]}
 volume=${volume:1:${#volume}-2}
-mapfile -t signal -ra signal <<< "$(</proc/net/wireless)"
+
+battery=$(</sys/class/power_supply/BAT0/capacity)%
+
+brightness=$(($(</sys/class/backlight/*/actual_brightness)*100/$(</sys/class/backlight/*/max_brightness)))%
+
+mapfile -t signal -ra signal < /proc/net/wireless
 signal=${signal[2]}
 IFS=' ' read -ra signal <<< $signal
 signal=${signal[2]}
 signal=${signal:0:-1}
-signal=$((signal*100/70))
+signal=$((signal*100/70))%
+
+read -r date < /proc/driver/rtc
+date=${date/rtc_time	: /}
+
 clr1="\e[37m"
 clr2="\e[32m"
+
 echo -e "
-$(uname -sr)
+$clr1 $system
 $clr1 Up:$clr2 $uptime
-$clr1 Proc:$clr2 ${topline2[1]}
-$clr1 Active:$clr2 ${topline2[3]}
-$clr1 Cpu:$clr2 $((100-$cpuidle))%
-$clr1 Mem:$clr2 $((${topline4[7]}/1024)) MiB / $((${topline4[3]}/1024)) MiB
-$clr1 Net in:$clr2 $((${netdev[1]}/1048576)) MiB
-$clr1 Net out:$clr2 $((${netdev[9]}/1048576)) MiB
+$clr1 Proc:$clr2 $processes
+$clr1 Active:$clr2 $activeprocesses
+$clr1 Cpu:$clr2 $cpuusage
+$clr1 Mem:$clr2 $meminfo
+$clr1 Net in:$clr2 $netin
+$clr1 Net out:$clr2 $netout
+$clr1 Battery:$clr2 $battery
+$clr1 Brightness:$clr2 $brightness
 $clr1 Volume:$clr2 $volume
-$clr1 Battery:$clr2 $(</sys/class/power_supply/BAT0/capacity)%
-$clr1 Brightness:$clr2 $(($(</sys/class/backlight/*/actual_brightness)*100/$(</sys/class/backlight/*/max_brightness)))%
-$clr1 Wifi:$clr2 $signal%
-$clr1 ${topline1[2]}
+$clr1 Wifi:$clr2 $signal
+$clr1 $date
        	\r" | tr -d '\n'
+sleep 1
 done
