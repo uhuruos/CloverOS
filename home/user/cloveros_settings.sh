edcfb3c298d94f3b11c1df5ef35d1f5d37461bd7
James Bond <jamesbond@007.uk>
2017-07-27 20:10:58 -0400

    cloveros_settings.sh: add spacing in message for emerge source toggle

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -75,10 +75,10 @@ case "$choice" in
 		if grep -q 'EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"' /etc/portage/make.conf; then
 			sudo sed -i 's/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2"/' /etc/portage/make.conf
 			echo -e "\nemerge will now install from source."
-			echo -e "\nTip: Copy over build settings from CloverOS:"
+			echo -e "\nTip: Copy over build settings from CloverOS:\n"
 			echo "sudo wget $gitprefix/binhost_settings/etc/portage/package.use -P /etc/portage/package.use/"
 			echo "sudo sh -c 'curl -s $gitprefix/binhost_settings/etc/portage/make.conf | grep '^USE=' >> /etc/portage/make.conf'"
-			echo 'And comment out ACCEPT_KEYWORDS="~amd64" from /etc/portage/make.conf'
+			echo -e '\nAnd comment out ACCEPT_KEYWORDS="~amd64" from /etc/portage/make.conf'
 		else
 			sudo sed -i 's/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2"/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"/' /etc/portage/make.conf
 			echo -e "\nemerge will now install from binary."
