522ba4de8189cc89324d464081a9f11fc87d98a0
James Bond <jamesbond@007.uk>
2017-08-01 21:52:09 -0400

    chroot_build_binhost.sh: rebuild openssl/openssh first

--- chroot_build_binhost.sh
+++ chroot_build_binhost.sh
@@ -29,6 +29,7 @@ mkdir /etc/portage/env
 wget https://gitgud.io/cloveros/cloveros/raw/master/binhost_settings/etc/portage/env/{no-lto,no-lto-graphite,no-lto-o3,no-lto-ofast,no-o3,no-ofast,size} -P /etc/portage/env/
 wget https://gitgud.io/cloveros/cloveros/raw/master/binhost_settings/var/lib/portage/world -O /var/lib/portage/world
 
+CFLAGS="-Ofast -mssse3 -pipe -flto=8 -funroll-loops" emerge openssl openssh
 CFLAGS="-Ofast -mssse3 -pipe -flto=8 -funroll-loops" emerge gcc layman
 
 yes | layman -a 0x4d4c bobwya deadbeef-overlay elementary eroen palemoon science steam-overlay torbrowser vapoursynth
