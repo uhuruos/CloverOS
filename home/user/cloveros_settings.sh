bc1e4ea82d3950e5d1ca30c2c84e73de2e5fcf3e
James Bond <jamesbond@007.uk>
2017-08-02 20:02:27 -0400

    cloveros_settings.sh: ask if you want to inherit binhost USE flags

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -75,11 +75,11 @@ case "$choice" in
 		if grep -q 'EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"' /etc/portage/make.conf; then
 			sudo sed -i 's/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"/EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2"/' /etc/portage/make.conf
 			echo -e "\nemerge will now install from source."
-			echo -e "\nCopy over binhost build settings? (USE flags /etc/portage/make.conf, /etc/portage/package.use/package.use)"
-			read -erp "Select option: " -n 1 binhostyn
+			echo -e "\n"
+			read -erp "Copy over binhost build settings? (USE flags: /etc/portage/make.conf, /etc/portage/package.use/package.use) [y/n]" -n 1 binhostyn
 			if [[ $binhostyn != "y" ]]; then
 				sudo wget $gitprefix/binhost_settings/etc/portage/package.use -O /etc/portage/package.use/package.use
-				sudo sh -c 'curl -s $gitprefix/binhost_settings/etc/portage/make.conf | grep '^USE=' >> /etc/portage/make.conf'
+				sudo sh -c 'curl -s $gitprefix/binhost_settings/etc/portage/make.conf | grep '^USE=' >> /etc/portage/make.conf
 				sudo sed -i 's/ACCEPT_KEYWORDS="~amd64"/#ACCEPT_KEYWORDS="~amd64"/' /etc/portage/make.conf
 			fi
 		else
