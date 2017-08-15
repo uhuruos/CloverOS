e066fdeba930088f97b4f066683bd0f9834e757b
James Bond <jamesbond@007.uk>
2017-08-15 13:07:27 -0400

    binhost_update.sh: remove /s/signatures/s/signatures/

--- binhost_update.sh
+++ binhost_update.sh
@@ -14,12 +14,14 @@ cd /usr/portage/packages/s/
 EIX_LIMIT=0 eix --installed -F | grep -v "Available versions" | ansi2html > packages.html
 php website.php
 
-cd /usr/portage/packages/s/signatures/
 rm -Rf /usr/portage/packages/s/signatures/*
+cd /usr/portage/packages/s/signatures/
 find /usr/portage/packages/ -type d | sed 's#/usr/portage/packages/##' | parallel mkdir
 find /usr/portage/packages/ -type f | sed 's#/usr/portage/packages/##' | pv -qB 1G | parallel gpg --armor --detach-sign --output {}.asc --sign /usr/portage/packages/{}
 
 chmod -R 755 /usr/portage/packages/
+rmdir /usr/portage/packages/s/signatures/s/signatures/
+
 rsync -av --delete-after /usr/portage/packages/ root@useast.cloveros.ga:/var/www/htdocs/useast.cloveros.ga/ &
 rsync -av --delete-after /usr/portage/packages/ root@uswest.cloveros.ga:/var/www/htdocs/uswest.cloveros.ga/ &
 rsync -av --delete-after /usr/portage/packages/ root@fr.cloveros.ga:/var/www/html/ &
