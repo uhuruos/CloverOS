f2c1b9237fd40a7a452b87b52b0c338030ba7ccc
James Bond <jamesbond@007.uk>
2017-08-17 19:08:44 -0400

    kernel_build.sh: added kernel config

--- kernel_build.sh
+++ kernel_build.sh
@@ -3,7 +3,7 @@ wget https://liquorix.net/sources/4.12/config.amd64
 genkernel --kernel-config=config.amd64 all
 dracut --xz
 cd /boot/
-tar -Jcvf ~/kernel.tar.xz *4.12.5*
+tar -cvf /usr/portage/packages/s/kernel.tar.xz *4.12.5*
 cd /lib/modules/
-tar -Jcvf ~/modules.tar.xz *4.12.5*
+tar -cvf /usr/portage/packages/s/modules.tar.xz *4.12.5*
 emerge -1 --jobs=1 zfs-kmod virtualbox-modules exfat-nofuse
