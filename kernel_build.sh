574a4bec922f733ff1c1f82d243b2eaa05d7912e
James Bond <jamesbond@007.uk>
2017-09-01 18:11:04 -0400

    cloveros_settings.sh: changed kernel upgrade method

--- kernel_build.sh
+++ kernel_build.sh
@@ -2,6 +2,10 @@ emerge gentoo-sources genkernel
 wget https://liquorix.net/sources/4.12/config.amd64
 genkernel --btrfs --zfs --lvm --mdadm --dmraid --iscsi --luks --kernel-config=Desktop/config.amd64 all
 
-tar -cvf /usr/portage/packages/s/kernel.tar.xz /boot/initramfs-genkernel-*-gentoo /boot/kernel-genkernel-*-gentoo /boot/System.map-genkernel-*-gentoo /lib/modules/*-gentoo/
+cd /boot/
+tar -cvf /usr/portage/packages/s/kernel.tar initramfs-genkernel-*-gentoo kernel-genkernel-*-gentoo System.map-genkernel-*-gentoo
+cd /lib/modules
+tar -rvf /usr/portage/packages/s/kernel.tar *-gentoo/
+xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
 
 emerge -1 --jobs=1 zfs-kmod virtualbox-modules exfat-nofuse
