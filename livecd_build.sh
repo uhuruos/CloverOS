4f75aa7677a6e1ae95d96ca6e4aa14ef615cbc4e
James Bond <jamesbond@007.uk>
2017-09-04 23:08:20 -0400

    installscript.sh, livecd_build.sh, livecd_install.sh, cloveros_settings.sh: change gpgvalidate.sh location

--- livecd_build.sh
+++ livecd_build.sh
@@ -32,9 +32,13 @@ EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y --jobs=2 -G"
 CFLAGS="-O3 -march=native -pipe -funroll-loops -floop-block -floop-interchange -floop-strip-mine -ftree-loop-distribution"
 CXXFLAGS="\${CFLAGS}"
 CPU_FLAGS_X86="mmx mmxext sse sse2 ssse3 sse3"
+
 PORTAGE_BINHOST="https://cloveros.ga"
-FETCHCOMMAND_HTTPS="/home/livecd/gpgvalidate.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""
+FETCHCOMMAND_HTTPS="/var/tmp/gpgvalidate.sh \"\${URI}\" \"\${DISTDIR}/\${FILE}\""
 ACCEPT_KEYWORDS="~amd64"' >> /etc/portage/make.conf
+
+wget https://gitgud.io/cloveros/cloveros/raw/master/home/user/gpgvalidate.sh -O /var/tmp/gpgvalidate.sh
+chmod +x gpgvalidate.sh
 
 #emerge gentoo-sources genkernel
 #wget http://liquorix.net/sources/4.9/config.amd64
@@ -95,8 +99,6 @@ wget https://gitgud.io/cloveros/cloveros/raw/master/home/user/stats.sh
 chmod +x stats.sh
 wget https://gitgud.io/cloveros/cloveros/raw/master/home/user/rotate_screen.sh
 chmod +x rotate_screen.sh
-wget https://gitgud.io/cloveros/cloveros/raw/master/home/user/gpgvalidate.sh
-chmod +x gpgvalidate.sh
 wget https://gitgud.io/cloveros/cloveros/raw/master/home/user/.emacs
 mkdir -p .emacs.d/backups
 mkdir .emacs.d/autosaves
