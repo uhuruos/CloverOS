dbf44d136d4ff962fc4f777617f46309512d5c34
James Bond <jamesbond@007.uk>
2017-10-14 13:12:07 -0400

    livecd_build.sh: cleanup

--- kernel_build.sh
+++ kernel_build.sh
@@ -4,7 +4,7 @@ genkernel --kernel-config=config.amd64 all
 
 rm /usr/portage/packages/s/kernel.tar.xz
 cd /boot/
-tar -cvf /usr/portage/packages/s/kernel.tar initramfs-genkernel-*-gentoo kernel-genkernel-*-gentoo System.map-genkernel-*-gentoo
+tar -cvf /usr/portage/packages/s/kernel.tar *-gentoo
 cd /lib/modules
 tar -rvf /usr/portage/packages/s/kernel.tar *-gentoo/
 xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
