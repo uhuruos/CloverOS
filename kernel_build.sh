1d54d3755ba6c6b6bc059c49721166efa1aab60a
James Bond <jamesbond@007.uk>
2017-10-14 00:24:49 -0400

    kernel_build.sh: use 8 threads

--- kernel_build.sh
+++ kernel_build.sh
@@ -7,6 +7,6 @@ cd /boot/
 tar -cvf /usr/portage/packages/s/kernel.tar initramfs-genkernel-*-gentoo kernel-genkernel-*-gentoo System.map-genkernel-*-gentoo
 cd /lib/modules
 tar -rvf /usr/portage/packages/s/kernel.tar *-gentoo/
-xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
+xz -9e -T 0 --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
 
 emerge -1 --jobs=1 zfs-kmod virtualbox-modules exfat-nofuse
