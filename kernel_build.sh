b9c4b088ac80f43ca2253a860310c07bb9d611b0
James Bond <jamesbond@007.uk>
2017-11-21 17:53:53 -0500

    binhost_settings: update

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,12 +1,30 @@
 emerge gentoo-sources genkernel
+cd /usr/src/linux/
 wget https://liquorix.net/sources/4.13/config.amd64
 genkernel --kernel-config=config.amd64 all
 
+kernelversion=4.14.1
+
 rm /usr/portage/packages/s/kernel.tar.xz
 cd /boot/
-tar -cvf /usr/portage/packages/s/kernel.tar *-gentoo
+tar -cvf /usr/portage/packages/s/kernel.tar *$kernelversion-gentoo
 cd /lib/modules
-tar -rvf /usr/portage/packages/s/kernel.tar *-gentoo/
+tar -rvf /usr/portage/packages/s/kernel.tar *$kernelversion-gentoo/
 xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
 
+cd /usr/src/linux/
+wget https://linux-libre.fsfla.org/pub/linux-libre/releases/4.14.1-gnu/deblob-4.14
+wget https://linux-libre.fsfla.org/pub/linux-libre/releases/4.14.1-gnu/deblob-check
+chmod +x deblob-4.14
+PYTHON="python2.7" ./deblob-4.14
+genkernel --kernel-config=config.amd64 all
+
+rm /usr/portage/packages/s/kernel-libre.tar.xz
+cd /boot/
+tar -cvf /usr/portage/packages/s/kernel-libre.tar *$kernelversion-gentoo-gnu
+cd /lib/modules
+tar -rvf /usr/portage/packages/s/kernel-libre.tar *$kernelversion-gentoo-gnu/
+xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel-libre.tar
+
 emerge -1 --jobs=1 zfs-kmod virtualbox-modules exfat-nofuse
+
