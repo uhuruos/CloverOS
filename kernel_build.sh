da748dcfcfeecef89bc57cb7e225da1ebf568657
James Bond <jamesbond@007.uk>
2017-12-22 15:26:47 -0500

    binhost_settings: update

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,7 +1,11 @@
 kernelversion=4.14.8
 kernelmajversion=4.14
+revision=-r1
 
-emerge gentoo-sources genkernel
+#emerge gentoo-sources genkernel
+
+binutils-config --linker ld.bfd
+
 cd /usr/src/linux/
 eselect kernel set 1
 wget https://liquorix.net/sources/4.14/config.amd64
@@ -9,29 +13,31 @@ genkernel --kernel-config=config.amd64 all
 
 rm /usr/portage/packages/s/kernel.tar.xz
 cd /boot/
-tar -cvf /usr/portage/packages/s/kernel.tar *$kernelversion-gentoo
+tar -cvf /usr/portage/packages/s/kernel.tar *$kernelversion-gentoo$revision
 cd /lib/modules
-tar -rvf /usr/portage/packages/s/kernel.tar *$kernelversion-gentoo/
+tar -rvf /usr/portage/packages/s/kernel.tar *$kernelversion-gentoo$revision/
 xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
 
 cd /usr/src/
-cp -R linux-$kernelversion-gentoo linux-$kernelversion-gentoo-gnu
-cd linux-$kernelversion-gentoo-gnu
+cp -R linux-$kernelversion-gentoo$revision linux-$kernelversion-gentoo-gnu$revision
+cd linux-$kernelversion-gentoo-gnu$revision
 wget https://linux-libre.fsfla.org/pub/linux-libre/releases/$kernelversion-gnu/deblob-$kernelmajversion
 wget https://linux-libre.fsfla.org/pub/linux-libre/releases/$kernelversion-gnu/deblob-check
 chmod +x deblob-$kernelmajversion
 PYTHON="python2.7" ./deblob-$kernelmajversion
 wget https://liquorix.net/sources/4.14/config.amd64
-genkernel --kernel-config=config.amd64 --kerneldir=/usr/src/linux-$kernelversion-gentoo-gnu all
+genkernel --kernel-config=config.amd64 --kerneldir=/usr/src/linux-$kernelversion-gentoo-gnu$revision all
 
 rm /usr/portage/packages/s/kernel-libre.tar.xz
 cd /boot/
-tar -cvf /usr/portage/packages/s/kernel-libre.tar *$kernelversion-gentoo-gnu
+tar -cvf /usr/portage/packages/s/kernel-libre.tar *$kernelversion-gentoo$revision-gnu
 cd /lib/modules
-tar -rvf /usr/portage/packages/s/kernel-libre.tar *$kernelversion-gentoo-gnu/
+tar -rvf /usr/portage/packages/s/kernel-libre.tar *$kernelversion-gentoo$revision-gnu/
 xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel-libre.tar
 
 cd /usr/src/linux/
 make prepare
 make modules_prepare
 emerge --buildpkg @module-rebuild @x11-module-rebuild
+
+binutils-config --linker ld.gold
