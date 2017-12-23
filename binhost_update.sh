dd9304553524d62fc347049fc5b65be06f9b6b97
James Bond <jamesbond@007.uk>
2017-12-23 09:12:18 -0500

    binhost_settings: update

--- binhost_update.sh
+++ binhost_update.sh
@@ -2,7 +2,7 @@ emerge --sync
 layman -S
 emerge -uvDN --with-bdeps=y --buildpkg @world
 emerge -1 --buildpkg $(eix -Jc | grep 9999 | cut -d" " -f2 | tr "\n" " ")
-emerge -C hwinfo ntfs3g && emerge --jobs=1 --buildpkg ntfs3g hwinfo
+emerge -C hwinfo ntfs3g && emerge --buildpkg ntfs3g && emerge --buildpkg hwinfo
 emerge --buildpkg @preserved-rebuild
 emerge --depclean
 
