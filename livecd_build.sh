ba5b98a76907dafee8f2bb0b3d1dfa3a6aa7f5bb
James Bond <jamesbond@007.uk>
2018-02-05 20:21:15 -0500

    change aria2c settings

--- livecd_build.sh
+++ livecd_build.sh
@@ -41,7 +41,7 @@ ACCEPT_KEYWORDS="**"' >> /etc/portage/make.conf
 emerge gnupg aria2
 gpg --keyserver keys.gnupg.net --recv-key "78F5 AC55 A120 07F2 2DF9 A28A 78B9 3F76 B8E4 2805"
 echo 'binhost_mirrors=\$PORTAGE_BINHOST",https://useast.cloveros.ga,https://fr.cloveros.ga,https://au.cloveros.ga,https://ca.cloveros.ga,https://uswest.cloveros.ga,https://uk.cloveros.ga,https://fr2.cloveros.ga,"
-FETCHCOMMAND_HTTPS="sh -c \"aria2c -x2 -s99 -j99 -k5M --dir \"\\\${DISTDIR}\" -o \"\\\${FILE}\" \\\\\\\$(sed -e \"s#,#\"\\\${DISTDIR}/\\\${FILE}\"\"\ \"#g\" -e \"s#"\$PKGDIR"##g\" -e \"s#.partial##g\" <<< "\$binhost_mirrors") && aria2c --dir \"\\\${DISTDIR}\" -o \"\\\${FILE}.asc\" \\\\\\\$(sed \"s#cloveros.ga/#cloveros.ga/s/signatures/#\" <<< \"\\\${URI}.asc\") && gpg --verify \"\\\${DISTDIR}/\\\${FILE}.asc\" \"\\\${DISTDIR}/\\\${FILE}\"\""' >> /etc/portage/make.conf
+FETCHCOMMAND_HTTPS="sh -c \"aria2c -x2 -s99 -j99 -k1M --dir \"\\\${DISTDIR}\" -o \"\\\${FILE}\" \\\\\\\$(sed -e \"s#,#\"\\\${DISTDIR}/\\\${FILE}\"\"\ \"#g\" -e \"s#"\$PKGDIR"##g\" -e \"s#.partial##g\" <<< "\$binhost_mirrors") && aria2c --dir \"\\\${DISTDIR}\" -o \"\\\${FILE}.asc\" \\\\\\\$(sed \"s#cloveros.ga/#cloveros.ga/s/signatures/#\" <<< \"\\\${URI}.asc\") && gpg --verify \"\\\${DISTDIR}/\\\${FILE}.asc\" \"\\\${DISTDIR}/\\\${FILE}\"\""' >> /etc/portage/make.conf
 
 #emerge gentoo-sources genkernel
 #wget http://liquorix.net/sources/4.14/config.amd64
