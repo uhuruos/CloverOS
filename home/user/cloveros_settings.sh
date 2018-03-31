ca9ef915b5ee516dbbf66537c5adc4da4603d50a
James Bond <jamesbond@007.uk>
2018-03-31 06:39:23 -0400

    spacefm

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -131,6 +131,7 @@ case "$choice" in
 		wget -q "$gitprefix"/home/user/.config/nitrogen/nitrogen.cfg -P .config/nitrogen
 		sed -i "s@/home/user/@/home/$USER/@" .config/nitrogen/nitrogen.cfg
 		sed -i "s@/home/user/@/home/$USER/@" .rtorrent.rc
+		sed -i "s@/home/user/@/home/$USER/@" .config/spacefm/session
 		mkdir .mpv
 		wget -q "$gitprefix"/home/user/.mpv/config -P .mpv
 		echo -e "\nConfiguration updated to new CloverOS defaults, old settings are moved to ~/$backupdir/ (~)"
