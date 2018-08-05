215a441cad45a4c4f8b21d08918be94cea9e1512
James Bond <jamesbond@007.uk>
2018-08-05 17:46:26 -0400

    cloveros_settings.sh: silence rm output

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -183,7 +183,7 @@ case "$choice" in
 			exit 1
 		fi
 
-		sudo rm /usr/portage/packages/Packages
+		sudo rm /usr/portage/packages/Packages &> /dev/null
 
 		sudo eselect profile set "default/linux/amd64/17.0/hardened"
 
