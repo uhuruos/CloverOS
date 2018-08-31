4ba7445f79f8595c3f1c26f80667d586249f5002
James Bond <jamesbond@007.uk>
2018-08-31 00:57:13 -0400

    stats.sh: choose network device

--- home/user/stats.sh
+++ home/user/stats.sh
@@ -2,39 +2,49 @@
 cpulasttotal=0
 cpulastidle=0
 
-if [[ -f ~/.asoundrc ]]; then
-    read -r alsadevice < ~/.asoundrc
-    alsadevice=${alsadevice/defaults.pcm.card /}
-    if [ $alsadevice -eq $alsadevice ] 2> /dev/null; then
-        :
-    else
-        alsadevice=0
-    fi
-else
-    alsadevice=0
-fi
+mapfile -t netdev < /proc/net/dev
 i=0
-if [[ -f /proc/asound/card$alsadevice/codec#0 ]]; then
-    while read line; do
-        if [[ $line == 'Amp-Out vals:  '* ]]; then
-            alsaline=$i
-            break
-        fi
-        ((i++))
-    done < /proc/asound/card$alsadevice/codec#0
-else
-    volume='N/A'
-fi
+for line in $netdev; do
+	IFS=' ' read -a netdevice <<< $line
+	if [[ ${netdev[0]} !== "lo" ]]; then
+		netdevice=$i
+	fi
+	((i++))
+done
 
 if [[ ! -f /sys/class/power_supply/BAT0/capacity ]]; then
-    battery='N/A'
+	battery='N/A'
 fi
 
 if compgen -G /sys/class/backlight/* > /dev/null; then
-    backlightdevice=(/sys/class/backlight/*)
-    backlightdevice=${backlightdevice[-1]}
+	backlightdevice=(/sys/class/backlight/*)
+	backlightdevice=${backlightdevice[-1]}
 else
-    brightness='N/A'
+	brightness='N/A'
+fi
+
+if [[ -f ~/.asoundrc ]]; then
+	read alsadevice < ~/.asoundrc
+	alsadevice=${alsadevice/defaults.pcm.card /}
+	if [ $alsadevice -eq $alsadevice ] 2> /dev/null; then
+		:
+	else
+		alsadevice=0
+	fi
+else
+	alsadevice=0
+fi
+i=0
+if [[ -f /proc/asound/card$alsadevice/codec#0 ]]; then
+	while read line; do
+	if [[ $line == 'Amp-Out vals:  '* ]]; then
+		alsaline=$i
+		break
+	fi
+	((i++))
+	done < /proc/asound/card$alsadevice/codec#0
+else
+	volume='N/A'
 fi
 
 while :
@@ -48,13 +58,13 @@ uptime=${uptime%%.*}
 hrs=$((uptime/3600))
 min=$((uptime/60%60))
 if [[ ${#min} -eq 1 ]]; then
-    min=0$min
+	min=0$min
 fi
 uptime="$hrs:$min"
 
 processes=()
 for line in /proc/*[0-9]; do
-    processes+=($line)
+	processes+=($line)
 done
 processes=${#processes[@]}
 
@@ -62,12 +72,12 @@ mapfile -t procstat < /proc/stat
 activeprocesses=${procstat[-3]}
 activeprocesses=${activeprocesses:14}
 
-IFS=' ' read -ra cpustats <<< ${procstat[0]}
+IFS=' ' read -a cpustats <<< ${procstat[0]}
 cpustats=("${cpustats[@]:1}")
 cpuidle=${cpustats[3]}
 cputotal=0
 for i in "${cpustats[@]}"; do
-    cputotal=$(($cputotal+$i))
+	cputotal=$(($cputotal+$i))
 done
 cpuusage=$(((1000*(($cputotal-$cpulasttotal)-($cpuidle-$cpulastidle))/($cputotal-$cpulasttotal)+5)/10))%
 cpulasttotal=$cputotal
@@ -84,23 +94,23 @@ memory[3]=${meminfo[20]} #shmem
 memory[4]=${meminfo[22]} #sreclaimable
 memused=$memtotal
 for line in "${memory[@]}"; do
-    line=${line#* }
-    line=${line/ kB/}
-    memused=$(($memused-$line))
+	line=${line#* }
+	line=${line/ kB/}
+	memused=$(($memused-$line))
 done
 meminfo=$(($memused/1024))\ MiB\ \/\ $(($memtotal/1024))\ MiB
 
 mapfile -t netdev < /proc/net/dev
-netdev=${netdev[-1]}
-IFS=' ' read -ra netdev <<< ${netdev}
+IFS=' ' read -a netdev <<< ${netdev[$netdevice]}
 netin=$((${netdev[1]}/1048576))\ MiB
 netout=$((${netdev[9]}/1048576))\ MiB
 
 if [[ $volume != 'N/A' ]]; then
-    mapfile -t asound < /proc/asound/card$alsadevice/codec#0
-    volume=${asound[$alsaline]}
-    volume=${volume:20:2}
-    volume=$((16#$volume))%
+
+mapfile -t asound < /proc/asound/card$alsadevice/codec#0
+	volume=${asound[$alsaline]}
+	volume=${volume:20:2}
+	volume=$((16#$volume))%
 fi
 
 ac=$(</sys/class/power_supply/AC/online)
@@ -111,22 +121,22 @@ else
 fi
 
 if [[ $battery != 'N/A' ]]; then
-    battery=$(</sys/class/power_supply/BAT0/capacity)%
+	battery=$(</sys/class/power_supply/BAT0/capacity)%
 fi
 
 if [[ $brightness != 'N/A' ]]; then
-    brightness=$(($(<$backlightdevice/actual_brightness)*100/$(<$backlightdevice/max_brightness)))%
+	brightness=$(($(<$backlightdevice/actual_brightness)*100/$(<$backlightdevice/max_brightness)))%
 fi
 
 mapfile -t signal -ra signal < /proc/net/wireless
 if [[ ${#signal[@]} -gt 2 ]]; then
-    signal=${signal[2]}
-    IFS=' ' read -ra signal <<< $signal
-    signal=${signal[2]}
-    signal=${signal:0:-1}
-    signal=$((signal*100/70))%
+	signal=${signal[2]}
+	IFS=' ' read -a signal <<< $signal
+	signal=${signal[2]}
+	signal=${signal:0:-1}
+	signal=$((signal*100/70))%
 else
-    signal='N/A'
+	signal='N/A'
 fi
 
 date=$(printf '%(%c)T')
