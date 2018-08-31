3c6a812e6cfb0c42419af31edbfc74558d7ca203
James Bond <jamesbond@007.uk>
2018-08-31 01:12:56 -0400

    cleanup

--- binhost_build_chroot_32bit.sh
+++ binhost_build_chroot_32bit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+if [ $(id -u) != '0' ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
 fi
 
 mkdir gentoo
