85fa34ee2bdfdfbc46be0296e7511227d9c37d4a
James Bond <jamesbond@007.uk>
2018-09-13 02:29:03 -0400

    cloveros_settings.sh c): fix quote

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -297,7 +297,7 @@ case "$choice" in
 		sudo wget $gitprefix/binhost_settings/etc/portage/env/no-lto $gitprefix/binhost_settings/etc/portage/env/no-lto-graphite $gitprefix/binhost_settings/etc/portage/env/no-lto-graphite-ofast $gitprefix/binhost_settings/etc/portage/env/no-lto-o3 $gitprefix/binhost_settings/etc/portage/env/no-lto-ofast $gitprefix/binhost_settings/etc/portage/env/no-o3 $gitprefix/binhost_settings/etc/portage/env/no-ofast $gitprefix/binhost_settings/etc/portage/env/size $gitprefix/binhost_settings/etc/portage/env/no-gcc -P /etc/portage/env/
 		useflags=$(curl -s $gitprefix/binhost_settings/etc/portage/make.conf | grep "^USE=")
 		if ! grep -q "$useflags" /etc/portage/make.conf; then
-			sudo sh -c "echo \"$useflags\" >> /etc/portage/make.conf"
+			sudo sh -c 'echo "$useflags" >> /etc/portage/make.conf'
 		fi
 		echo -e "\nPortage configuration now mirrors binhost Portage configuration. Previous Portage config stored in ~/$backupportagedir"
 		;;
