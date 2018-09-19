c651ebc6356fa4a8fbfcb1c54ec98505d60b12c4
James Bond <jamesbond@007.uk>
2018-09-19 14:12:56 -0400

    kernel_build.sh: shorten and clean

--- kernel_build.sh
+++ kernel_build.sh
@@ -17,10 +17,6 @@ echo -e "CONFIG_SND_HDA_INPUT_BEEP=y\nCONFIG_SND_HDA_INPUT_BEEP_MODE=0" >> confi
 genkernel --kernel-config=config.amd64 --luks --lvm all
 grub-mkconfig -o /boot/grub/grub.cfg
 
-tar -cf /usr/portage/packages/s/kernel.tar -C /boot/ *$kernelversion-gentoo
-tar -rf /usr/portage/packages/s/kernel.tar -C /lib/modules/ *$kernelversion-gentoo/
-xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
-
 cp -R /usr/src/linux-$kernelversion-gentoo/ /usr/src/linux-$kernelversion-gentoo-gnu/
 wget https://linux-libre.fsfla.org/pub/linux-libre/releases/$kernelversion-gnu/deblob-$kernelmajversion -P /usr/src/linux-$kernelversion-gentoo-gnu/
 wget https://linux-libre.fsfla.org/pub/linux-libre/releases/$kernelversion-gnu/deblob-check -P /usr/src/linux-$kernelversion-gentoo-gnu/
@@ -28,6 +24,9 @@ chmod +x /usr/src/linux-$kernelversion-gentoo-gnu/deblob-$kernelmajversion /usr/
 cd /usr/src/linux-$kernelversion-gentoo-gnu/ && PYTHON="python2.7" /usr/src/linux-$kernelversion-gentoo-gnu/deblob-$kernelmajversion && cd -
 genkernel --kernel-config=config.amd64 --kerneldir=/usr/src/linux-$kernelversion-gentoo-gnu/ --luks --lvm all
 
+tar -cf /usr/portage/packages/s/kernel.tar -C /boot/ *$kernelversion-gentoo
+tar -rf /usr/portage/packages/s/kernel.tar -C /lib/modules/ *$kernelversion-gentoo/
+xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel.tar
 tar -cf /usr/portage/packages/s/kernel-libre.tar -C /boot/ *$kernelversion-gentoo-gnu
 tar -rf /usr/portage/packages/s/kernel-libre.tar -C /lib/modules/ *$kernelversion-gentoo-gnu/
 xz -9e --lzma2=dict=256MB /usr/portage/packages/s/kernel-libre.tar
