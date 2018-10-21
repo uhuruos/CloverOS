c9653c17d4ac8c85809ae2ae317c8eff645528fa
James Bond <jamesbond@007.uk>
2018-10-21 13:49:08 -0400

    binhost_settings: update

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -284,7 +284,7 @@ case "$choice" in
 		portageworkdir=portageworkdir$(< /dev/urandom tr -dc 0-9 | head -c 8)
 		mkdir -p $portageworkdir/env/
 		wget $gitprefix/binhost_settings/etc/portage/{package.use,package.keywords,package.env,package.mask,package.unmask} -P $portageworkdir/
-		wget $gitprefix/binhost_settings/etc/portage/env/{no-lto,no-lto-graphite,no-lto-graphite-ofast,no-lto-o3,no-lto-ofast,no-o3,no-ofast,size,no-gcc,gold,pcsx2} -P $portageworkdir/env/
+		wget $gitprefix/binhost_settings/etc/portage/env/{no-lto,no-lto-graphite,no-lto-graphite-ofast,no-lto-o3,no-lto-ofast,no-o3,no-ofast,size,gold,pcsx2} -P $portageworkdir/env/
 		if [[ $(find $portageworkdir -type f | wc -l) == "16" ]]; then
 			backupportagedir=backupportage$(< /dev/urandom tr -dc 0-9 | head -c 8)
 			mkdir $backupportagedir
