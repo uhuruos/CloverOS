715cc6b3076cb9069f5033938ec277773bacd1c3
James Bond <jamesbond@007.uk>
2018-12-17 14:41:15 -0500

    home/user/stats.c: tweak

--- home/user/stats.c
+++ home/user/stats.c
@@ -199,7 +199,8 @@ void main(void) {
 		file = strchr(file, '\n')+1;
 		char wifi[5];
 		if (*file) {
-			for (int i = 0; i < 3; i++, file = strchr(file, ' ')+1);
+			strtok(file, " ");
+			for (int i = 0; i < 2; i++, file = strtok(NULL, " "));
 			file[strlen(file)-1] = '\0';
 			sprintf(wifi, "%d%%", atoi(file)*100/70);
 		} else {
