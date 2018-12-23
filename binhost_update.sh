357f52c79b963f1ba247c8e9b77d1f95fcfdfc05
James Bond <jamesbond@007.uk>
2018-12-23 04:16:24 -0500

    binhost_settings: update

--- binhost_update.sh
+++ binhost_update.sh
@@ -7,14 +7,14 @@ emerge --buildpkg @preserved-rebuild
 emerge --depclean
 emerge -1 --buildpkg $(find /var/db/pkg/ -mindepth 2 -maxdepth 2 -name \*-9999 | grep -v MERGING | awk -F \/ '{printf "=%s/%s ", $5, $6}')
 eclean-pkg
+PKGDIR="/usr/portage/packages/s/nodbus/" USE="-dbus -webengine -trash-panel-plugin" emerge --buildpkgonly glib qtgui PyQt5 thunar
 
 #mv /usr/portage/packages/s/ .
 #rm -Rf /usr/portage/packages/*
 #mv s/ /usr/portage/packages/
-#quickpkg --include-unmodified-config=y "*/*" 2>&1 | ansi2html | tail -n +1081 | head -n -7 > /usr/portage/packages/s/quickpkg.txt
-#emerge --buildpkgonly sudo openssh postfix dcron vixie-cron cronie fcron anacron \=nvidia-drivers-390.87 \=nvidia-drivers-304.137
+#quickpkg --include-unmodified-config=y "*/*" 2>&1 | ansi2html > /usr/portage/packages/s/quickpkg.txt
+#emerge --buildpkgonly sudo openssh postfix dcron vixie-cron cronie fcron anacron
 #emerge -C hwinfo ntfs3g && emerge --buildpkg ntfs3g && emerge --buildpkg hwinfo
-PKGDIR="/usr/portage/packages/s/nodbus/" USE="-dbus -webengine -trash-panel-plugin" emerge --buildpkgonly glib qtgui PyQt5 thunar
 
 cd /usr/portage/packages/s/
 php website.php
