cec3cd5676612c2b256bda58cdb7c0b0334a5b92
James Bond <jamesbond@007.uk>
2019-03-22 03:02:42 +0000

    clean

--- binhost_update.sh
+++ binhost_update.sh
@@ -10,21 +10,19 @@ if [ ! -d '/usr/portage/packages/s/signatures/' ]; then
 	mkdir -p /usr/portage/packages/s/signatures/
 fi
 
-#mv /usr/portage/packages/s/ .
-#rm -Rf /usr/portage/packages/*
-#mv s/ /usr/portage/packages/
 #quickpkg --include-unmodified-config=y "*/*" 2>&1 | ansi2html > /usr/portage/packages/s/quickpkg.txt
-#emerge -B sudo openssh postfix dcron vixie-cron cronie fcron anacron ungoogled-chromium
 #emerge -C hwinfo ntfs3g && emerge -b ntfs3g && emerge -b hwinfo
-#PKGDIR="/usr/portage/packages/s/nodbus/" USE="-dbus -webengine -trash-panel-plugin" emerge -B glib qtgui PyQt5 thunar
 
 emerge --sync
 layman -S
-emerge -uavDNb --exclude=gentoo-sources @world
+emerge -uavDN -b1 --exclude=gentoo-sources @world $(find /var/db/pkg/ -mindepth 2 -maxdepth 2 -name \*-9999 | awk -F \/ '{printf "=%s/%s ", $5, $6}')
 emerge -b @preserved-rebuild
 emerge --depclean
-emerge -1b $(find /var/db/pkg/ -mindepth 2 -maxdepth 2 -name \*-9999 | grep -v MERGING | awk -F \/ '{printf "=%s/%s ", $5, $6}')
-eclean-pkg
+eclean -d distfiles
+
+eclean -d packages
+emerge -B sudo openssh postfix dcron vixie-cron cronie fcron anacron ungoogled-chromium
+PKGDIR="/usr/portage/packages/s/nodbus/" USE="-dbus -webengine -trash-panel-plugin" emerge -B glib qtgui PyQt5 thunar
 
 php mirrors/index.php > /usr/portage/packages/index.html
 php mirrors/indexalt.php > /usr/portage/packages/indexalt.html
