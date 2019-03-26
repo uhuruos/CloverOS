334b03edd633b32272f37ef1cf8f0d7f7fe1957e
James Bond <jamesbond@007.uk>
2019-03-26 23:33:36 +0000

    kernel_build.sh: update kernel to 5.0.4

--- kernel_build.sh
+++ kernel_build.sh
@@ -4,8 +4,8 @@ if [ $(id -u) != "0" ]; then
 	exit 1
 fi
 
-kernelversion=4.19.31
-kernelmajversion=4.19
+kernelversion=5.0.4
+kernelmajversion=5.0
 
 if [ ! -d '/usr/portage/packages/s/' ]; then
 	mkdir -p /usr/portage/packages/s/
@@ -19,10 +19,10 @@ find /boot/ /lib/modules/ -mindepth 1 -maxdepth 1 -name \*gentoo\* ! -name \*$(u
 
 emerge =gentoo-sources-$kernelversion
 eselect kernel set linux-$kernelversion-gentoo
-wget https://liquorix.net/sources/$kernelmajversion/config.amd64
-sed -i "s/CONFIG_CRYPTO_CRC32C=m/CONFIG_CRYPTO_CRC32C=y/; s/CONFIG_FW_LOADER_USER_HELPER=y/CONFIG_FW_LOADER_USER_HELPER=n/" config.amd64
-echo -e "CONFIG_SND_HDA_INPUT_BEEP=y\nCONFIG_SND_HDA_INPUT_BEEP_MODE=0" >> config.amd64
-genkernel --kernel-config=config.amd64 --luks --lvm all
+wget https://raw.githubusercontent.com/damentz/liquorix-package/master/linux-liquorix/debian/config/kernelarch-x86/config-arch-64 -O config
+sed -i "s/CONFIG_CRYPTO_CRC32C=m/CONFIG_CRYPTO_CRC32C=y/; s/CONFIG_FW_LOADER_USER_HELPER=y/CONFIG_FW_LOADER_USER_HELPER=n/" config
+echo -e "CONFIG_SND_HDA_INPUT_BEEP=y\nCONFIG_SND_HDA_INPUT_BEEP_MODE=0" >> config
+genkernel --kernel-config=config --luks --lvm all
 XZ_OPT="--lzma1=preset=9e,dict=256MB,nice=273,depth=200,lc=4" tar --lzma -cf /usr/portage/packages/s/kernel.tar.lzma /boot/*$kernelversion-gentoo /lib/modules/$kernelversion-gentoo &
 
 cp -R /usr/src/linux-$kernelversion-gentoo/ /usr/src/linux-$kernelversion-gentoo-gnu/
@@ -30,12 +30,12 @@ wget https://linux-libre.fsfla.org/pub/linux-libre/releases/$kernelversion-gnu/d
 wget https://linux-libre.fsfla.org/pub/linux-libre/releases/$kernelversion-gnu/deblob-check -P /usr/src/linux-$kernelversion-gentoo-gnu/
 chmod +x /usr/src/linux-$kernelversion-gentoo-gnu/deblob-$kernelmajversion /usr/src/linux-$kernelversion-gentoo-gnu/deblob-check
 cd /usr/src/linux-$kernelversion-gentoo-gnu/ ; PYTHON="python2.7" /usr/src/linux-$kernelversion-gentoo-gnu/deblob-$kernelmajversion ; cd -
-genkernel --kernel-config=config.amd64 --kerneldir=/usr/src/linux-$kernelversion-gentoo-gnu/ --luks --lvm all
+genkernel --kernel-config=config --kerneldir=/usr/src/linux-$kernelversion-gentoo-gnu/ --luks --lvm all
 XZ_OPT="--lzma1=preset=9e,dict=256MB,nice=273,depth=200,lc=4" tar --lzma -cf /usr/portage/packages/s/kernel-libre.tar.lzma /boot/*$kernelversion-gentoo-gnu /lib/modules/$kernelversion-gentoo-gnu &
 
 wait
 
-rm config.amd64
+rm config
 cd /usr/src/linux/
 make clean
 make prepare
