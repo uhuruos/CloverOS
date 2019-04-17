2d2a3c6c050b90b77113efd54a69ea67890e11c8
James Bond <jamesbond@007.uk>
2019-04-17 06:05:43 +0000

    cloveros_settings.sh u): add fix for wine-any to wine-staging change

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -70,6 +70,10 @@ case "$choice" in
 		if [ ! -d /var/db/pkg/net-libs/gnutls-3.6.7/ ]; then
 			sudo FETCHCOMMAND_HTTPS="wget -O \"\${DISTDIR}/\${FILE}\" \"\${URI}\"" emerge -1 gnutls aria2
 		fi
+		if [ -d /var/db/pkg/app-emulation/wine-any-4.1/ ]; then
+			sudo emerge -C wine wine-any
+			sudo emerge wine
+		fi
 		if [ -d /var/db/pkg/net-p2p/rtorrent-ps-9999/ ]; then
 			sudo emerge -C rtorrent-ps
 			sudo emerge rtorrent
