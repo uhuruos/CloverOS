b973d3c46d4275c09115b3a73217223ee8b851b4
James Bond <jamesbond@007.uk>
2019-05-20 05:00:01 +0000

    binhost_build_chroot.sh: update/rewrite

--- binhost_build_chroot.sh
+++ binhost_build_chroot.sh
@@ -32,7 +32,9 @@ wget $gitprefix/binhost_settings/var/lib/portage/world -O /var/lib/portage/world
 emerge genkernel gentoo-sources
 eselect kernel set 1
 wget https://raw.githubusercontent.com/damentz/liquorix-package/5.0/linux-liquorix/debian/config/kernelarch-x86/config-arch-64
-genkernel --kernel-config=config-arch-64 all
+sed -i "s/CONFIG_CRYPTO_CRC32C=m/CONFIG_CRYPTO_CRC32C=y/; s/CONFIG_FW_LOADER_USER_HELPER=y/CONFIG_FW_LOADER_USER_HELPER=n/; s/CONFIG_I2C_NVIDIA_GPU=/#CONFIG_I2C_NVIDIA_GPU=/" config-arch-64
+echo -e "CONFIG_SND_HDA_INPUT_BEEP=y\nCONFIG_SND_HDA_INPUT_BEEP_MODE=0" >> config-arch-64
+genkernel --kernel-config=config-arch-64 --luks --lvm all
 (cd /usr/src/linux/ ; make clean ; make prepare ; make modules_prepare)
 binutils-config --linker ld.gold
 
