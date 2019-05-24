da61b5404c9b5f37371e745a8b51f1d002f9cc22
James Bond <jamesbond@007.uk>
2019-05-24 02:17:00 +0000

    cleanup

--- home/user/stats.sh
+++ home/user/stats.sh
@@ -263,8 +263,8 @@ void main(void) {
 
 		time_t rawtime = time(NULL);
 		struct tm *info = localtime(&rawtime);
-		char date[40];
-		strftime(date, 40, "%a %d %b %Y %H:%M:%S %Z", info);
+		char date[30];
+		strftime(date, 30, "%a %d %b %Y %H:%M:%S %Z", info);
 
 		printf("\e[?25l\e[37m%s Up: \e[32m%s\e[37m Proc: \e[32m%s\e[37m Active: \e[32m%s\e[37m Cpu: \e[32m%s\e[37m Mem: \e[32m%s\e[37m Net In: \e[32m%s (%s)\e[37m Net Out: \e[32m%s (%s)\e[37m Temp: \e[32m%s\e[37m Volume: \e[32m%s\e[37m AC: \e[32m%s\e[37m Battery: \e[32m%s\e[37m Brightness: \e[32m%s\e[37m Wifi: \e[32m%s\e[37m %s             \e[0m\r", uname, uptime, processes, active, cpu, memory, netin, totalin, netout, totalout, temperature, volume, ac, battery, brightness, wifi, date);
 		fflush(stdout);
