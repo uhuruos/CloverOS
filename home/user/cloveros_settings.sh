284e89cae92a2dbb8937b156aebd4a7267015d67
James Bond <jamesbond@007.uk>
2019-05-28 14:39:29 +0000

    cloveros_settings.sh: cleanup

--- home/user/cloveros_settings.sh
+++ home/user/cloveros_settings.sh
@@ -84,6 +84,8 @@ case "$choice" in
 		sudo emerge -uvD @world
 		sudo emerge --depclean
 
+		echo Removing dbus if possible...; for i in {dev-libs/glib,dev-qt/qtgui,dev-python/PyQt5}; do [ -d /var/db/pkg/$i* ] && sudo PORTAGE_BINHOST="https://cloveros.ga/s/nodbus" FETCHCOMMAND_HTTPS="wget -O \"\${DISTDIR}/\${FILE}\" \"\${URI}\"" emerge -1uD $i; done; sudo emerge --depclean
+
 		kernel=$(uname -r)
 		if [[ ${kernel: -3} == "gnu" ]]; then
 			./cloveros_settings.sh l
