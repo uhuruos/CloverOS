1f99f7d486efb107d31651d5cd22adfb60ab2707
James Bond <jamesbond@007.uk>
2019-06-06 11:34:56 +0000

    stats.sh: abbreviate names

--- home/user/stats.sh
+++ home/user/stats.sh
@@ -267,7 +267,7 @@ void main(void) {
 		char date[30];
 		strftime(date, 30, "%a %d %b %Y %H:%M:%S %Z", info);
 
-		printf("\e[?25l\e[37m%s Up: \e[32m%s\e[37m Proc: \e[32m%s\e[37m Active: \e[32m%s\e[37m Cpu: \e[32m%s\e[37m Mem: \e[32m%s\e[37m Net In: \e[32m%s (%s)\e[37m Net Out: \e[32m%s (%s)\e[37m Temperature: \e[32m%s\e[37m Volume: \e[32m%s\e[37m AC: \e[32m%s\e[37m Battery: \e[32m%s\e[37m Brightness: \e[32m%s\e[37m Wifi: \e[32m%s\e[37m %s             \e[0m\r", uname, uptime, processes, active, cpu, memory, netin, totalin, netout, totalout, temperature, volume, ac, battery, brightness, wifi, date);
+		printf("\e[?25l\e[37m%s Up: \e[32m%s\e[37m Proc: \e[32m%s\e[37m Active: \e[32m%s\e[37m Cpu: \e[32m%s\e[37m Mem: \e[32m%s\e[37m Net In: \e[32m%s (%s)\e[37m Net Out: \e[32m%s (%s)\e[37m Temp: \e[32m%s\e[37m Vol: \e[32m%s\e[37m AC: \e[32m%s\e[37m Batt: \e[32m%s\e[37m Bright: \e[32m%s\e[37m Wifi: \e[32m%s\e[37m %s             \e[0m\r", uname, uptime, processes, active, cpu, memory, netin, totalin, netout, totalout, temperature, volume, ac, battery, brightness, wifi, date);
 		fflush(stdout);
 		firstrun = 0;
 		nanosleep((struct timespec[]){{2, 0}}, NULL);
