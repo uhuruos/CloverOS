083922661509693e3ed9aad8b4bce1f183e0b8e7
James Bond <jamesbond@007.uk>
2019-06-24 14:29:52 +0000

    update

--- binhost_update.sh
+++ binhost_update.sh
@@ -16,7 +16,7 @@ if [ "$1" = 'deep' ]; then
 	find /usr/portage/packages/ -mindepth 1 -maxdepth 1 ! -name s -exec rm -Rf {} \; ; rm -Rf /usr/portage/packages/s/nodbus/
 	emerge -C hwinfo ntfs3g ; emerge ntfs3g ; emerge hwinfo ; emerge -C sys-apps/dbus obs-studio ; emerge obs-studio ; emerge -1 sys-apps/dbus ; emerge -C jack-audio-connection-kit audacity ; emerge audacity ; emerge jack-audio-connection-kit
 	quickpkg --include-unmodified-config=y "*/*" 2>&1 | ansi2html > /usr/portage/packages/s/quickpkg.html
-	emerge -B sudo openssh postfix dcron vixie-cron cronie fcron anacron ungoogled-chromium
+	emerge -B sudo openssh postfix dcron vixie-cron cronie fcron anacron
 	PKGDIR="/usr/portage/packages/s/nodbus/" USE="-dbus -webengine" emerge -B glib qtgui PyQt5
 fi
 
