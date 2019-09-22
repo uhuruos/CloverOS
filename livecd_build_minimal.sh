161847cd43677e168aab2fd5ebf099744ce33578
James Bond <jamesbond@007.uk>
2019-09-22 21:42:24 +0000

    livecd_build_minimal.sh: update

--- livecd_build_minimal.sh
+++ livecd_build_minimal.sh
@@ -54,6 +54,7 @@ gpasswd -a $username wheel
 PORTAGE_BINHOST="https://cloveros.ga/s/nodbus" FETCHCOMMAND_HTTPS="wget -O \"\\\${DISTDIR}/\\\${FILE}\" \"\\\${URI}\"" emerge -1O glib wpa_supplicant spacefm linux-firmware mesa
 emerge --noreplace wpa_supplicant spacefm linux-firmware
 emerge -eDv --exclude "glib wpa_supplicant spacefm linux-firmware mesa" @world xorg-server fvwm rxvt-unicode nitrogen compton sudo porthole rtorrent weechat alsa-utils zsh zsh-completions gentoo-zsh-completions liberation-fonts hack vlgothic scrot xbindkeys xinput arandr slock p7zip games-envd gparted squashfs-tools os-prober exfat-nofuse sshfs curlftpfs
+emerge -1O mesa
 emerge --depclean
 echo "frozen-files=\"/etc/sudoers\"" >> /etc/dispatch-conf.conf
 sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
