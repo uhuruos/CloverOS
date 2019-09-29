8cb261a769ba53f4ce6b81286455054d8b91a945
James Bond <jamesbond@007.uk>
2019-09-29 05:31:09 +0000

    binhost_settings: update

--- kernel_build.sh
+++ kernel_build.sh
@@ -4,8 +4,8 @@ if [ $(id -u) != "0" ]; then
 	exit 1
 fi
 
-kernelversion=5.3.1
-kernelmajversion=5.3
+kernelversion=5.2.17
+kernelmajversion=5.2
 
 if [ ! -d '/usr/portage/packages/s/' ]; then
 	mkdir -p /usr/portage/packages/s/
